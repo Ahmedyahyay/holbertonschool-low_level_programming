8694c80497e640f8a27698ad18c10aa2e945ca2d
Ahmedyahyay <ahmed@example.com>
2025-02-13 16:25:03 +0300

    compiler

---
+++ hello_world/1-main.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	    return (0);
+}
+julien@ubuntu:~/c/$ ./1-compiler 
+julien@ubuntu:~/c/$ ls
+0-preprocessor  1-compiler   c            main.o
+Makefile               100-intel      main.c  main.s
+julien@ubuntu:~/c/$ cat -v main.o | head
