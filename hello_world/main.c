681dce802406c97ed53997cf6ea423d203dd442a
Ahmedyahyay <ahmed@example.com>
2025-02-13 16:35:03 +0300

    modified main.c

--- hello_world/main.c
+++ hello_world/main.c
@@ -9,7 +9,3 @@ int main(void)
 {
 	    return (0);
 }
-julien@ubuntu:~/c/$ ./1-compiler 
-julien@ubuntu:~/c/$ ls
-0-preprocessor  1-compiler   c            main.o
-Makefile               100-intel      main.c  main.s
