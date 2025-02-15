b4b976e893b075571a80d4b58edb7bb1a074f19b
Ahmedyahyay <ahmed@example.com>
2025-02-15 13:30:49 +0300

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * print "Programming is like building a multilingual puzzle"
+ * return 0
+*/
+
+int main( ){
+puts("Programming is like building a multilingual puzzle");
+return 0;}
