2b926b73cae3f6a09c623839a19cc3a382a19798
Ahmedyahyay <ahmed@example.com>
2025-02-15 14:03:13 +0300

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - print "Programming is like building a multilingual puzzle
+ * main - prints "Programming is like building a multilingual puzzle
  * puzzle: followed by a newline.
- * Return: Always 0.
+ * Return: Always 0 (Success).
  */
  
 
-int main(void ){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void )
+
+{
+    puts("Programming is like building a multilingual puzzle/n");
+    return (0);
+
 }
