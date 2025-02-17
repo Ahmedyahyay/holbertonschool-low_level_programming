05f46b1c33b3e1d69a8e10da1b72b0a8be2a0c59
Ahmedyahyay <ahmed@example.com>
2025-02-17 18:50:21 +0300

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* function main -This will prints positive,negative or zero numbers*/
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
