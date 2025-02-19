a31b97f4a81b52fb566add84fa58bf697e6fe8d8
Ahmedyahyay <ahmed@example.com>
2025-02-19 20:16:05 +0300

    2-print_alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,15 @@
 	void print_alphabet_x10(void)
 
 {	int i;
-	char c;
+	char j;
 
 	for (i = 0; i < 10; i++)
 {
-	for (c = 'a'; c <= 'z'; c++)
-	_putchar(c);
-	_putchar('\n');
+	for (j = 'a'; j <= 'z'; j++)
+	{
+	_putchar(j);
+	}
+	 _putchar('\n');
 }
 }
 
