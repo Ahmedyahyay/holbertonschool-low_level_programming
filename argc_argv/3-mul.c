c2db31964b0d37d28323b4a176095a5168a19b3a
Ahmedyahyay <ahmed@example.com>
2025-03-07 19:32:55 +0300

    3-mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,19 +11,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, prod;
+	int n1, n2;
 
-	if (argc != 3)
+	if (argc == 3)
 	{
-		printf("Error\n");
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+
+	}
+	else
+	{
+	printf("Error\n");
 		return (1);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	prod = num1 * num2;
-
-	printf("%d\n", prod);
 
 	return (0);
 }
