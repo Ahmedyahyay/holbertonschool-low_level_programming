65a902af6a04def0419eee2cb08f7559527f876d
Ahmedyahyay <ahmed@example.com>
2025-03-15 20:09:37 +0300

    5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,11 +7,11 @@
  */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        free(d->owner);
-        free(d->name);
-        free(d);
-    }
+	if (d != NULL)
+	{
+	free(d->owner);
+	free(d->name);
+	free(d);
+	}
 }
 
